41d466fc9584728d86f90f0bc9d77a688d5ae1fa
ilyalukyanov34-al <ilyalukyanov34>
2026-06-29 21:37:59 +0500

    fix: set basename for BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.scss";
+// import { BrowserRouter } from "react-router-dom";
+
+// createRoot(document.getElementById("root")!).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+// );
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  // Добавляем basename, чтобы роутер учитывал подпапку GitHub Pages
+  <BrowserRouter basename="/Product-Catalog">
     <App />
   </BrowserRouter>,
 );
